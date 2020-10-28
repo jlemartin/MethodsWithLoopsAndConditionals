86c4b4bed15175f9feced082ab8f8e67d90626c3
John Martin <jlemartin7@gmail.com>
2020-10-27 20:21:27 -0500

    Luke Warm 1

---
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a method that will print to the console all numbers 1000 through -1000.
+            Countdown();
+
+
+        }
+
+        // Write a method that will print to the console all numbers 1000 through -1000.
+        public static void Countdown()
+        {
+            for (int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
